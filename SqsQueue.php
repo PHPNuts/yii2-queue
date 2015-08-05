e302d486f53f3b299b9b568d583ae45486003b6d
Alexander Kochetov <creocoder@gmail.com>
2015-08-05 23:57:13 +0300

    Added \yii\queue\SqsQueue

---
+++ SqsQueue.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\queue;
+
+use Aws\Sqs\SqsClient;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * SqsQueue
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
+class SqsQueue extends Component implements QueueInterface
+{
+    /**
+     * @var string
+     */
+    public $key;
+    /**
+     * @var string
+     */
+    public $secret;
+    /**
+     * @var string
+     */
+    public $region;
+    /**
+     * @var string
+     */
+    public $version = 'latest';
+    /**
+     * @var SqsClient
+     */
+    protected $client;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if ($this->key === null) {
+            throw new InvalidConfigException('The "key" property must be set.');
+        }
+
+        if ($this->secret === null) {
+            throw new InvalidConfigException('The "secret" property must be set.');
+        }
+
+        if ($this->region === null) {
+            throw new InvalidConfigException('The "region" property must be set.');
+        }
+
+        $this->client = new SqsClient([
+            'credentials' => [
+                'key' => $this->key,
+                'secret' => $this->secret,
+            ],
+            'region' => $this->region,
+            'version' => $this->version,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function push($payload, $queue, $delay = 0)
+    {
+        return $this->client->sendMessage([
+            'QueueUrl' => $queue,
+            'MessageBody' => $payload,
+            'DelaySeconds' => $delay,
+        ])->get('MessageId');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function pop($queue)
+    {
+        $response = $this->client->receiveMessage(['QueueUrl' => $queue]);
+
+        if (count($response['Messages']) < 1) {
+            return null;
+        }
+
+        $data = reset($response['Messages']);
+        $message = new Message(['id' => $data['MessageId'], 'payload' => $data['Body']]);
+        unset($data['MessageId'], $data['Body']);
+
+        if (!isset($data['QueueUrl'])) {
+            $data['QueueUrl'] = $queue;
+        }
+
+        $message->setParams($data);
+
+        return $message;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function purge($queue) {
+        $this->client->deleteQueue(['QueueUrl' => $queue]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function delete(Message $message)
+    {
+        $this->client->deleteMessage([
+            'QueueUrl' => $message->getParam('QueueUrl'),
+            'ReceiptHandle' => $message->getParam('ReceiptHandle'),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function release(Message $message, $delay = 0)
+    {
+        $this->client->changeMessageVisibility([
+            'QueueUrl' => $message->getParam('QueueUrl'),
+            'ReceiptHandle' => $message->getParam('ReceiptHandle'),
+            'VisibilityTimeout' => $delay,
+        ]);
+    }
+}
